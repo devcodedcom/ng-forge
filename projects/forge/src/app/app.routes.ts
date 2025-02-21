11628db37284b7e73be12f1cf269e111836211bb
Lukasz Fajger <lukasfajger@gmail.com>
2025-02-21 19:57:53 +0100

    Add folder structure

--- projects/forge/src/app/app.routes.ts
+++ projects/forge/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'english',
+    loadComponent: () => import('./feature/english/component/page/english/english.page').then(page => page.EnglishPage)
+  }
+];
---
+++ projects/forge/src/app/feature/english/component/page/english/english.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fg-english',
+  standalone: true,
+  templateUrl: './english.component.html',
+  styleUrl: './english.component.scss'
+})
+export class EnglishPage {
+
+}
