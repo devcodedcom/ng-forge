36e6f3d8bbb5bcb7bd85c86b65e3de412663cb83
Lukasz Fajger <lukasfajger@gmail.com>
2025-02-21 17:42:54 +0100

    Add ng-ui library

---
+++ projects/ng-ui/src/lib/ng-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-ng-ui',
+  imports: [],
+  template: `
+    <p>
+      ng-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgUiComponent {
+
+}
---
+++ projects/ng-ui/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-ui
+ */
+
+export * from './lib/ng-ui.service';
+export * from './lib/ng-ui.component';
