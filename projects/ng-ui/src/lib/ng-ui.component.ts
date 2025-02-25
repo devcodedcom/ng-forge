347661c599857e1680b044f499691456d4639219
Lukasz Fajger <lukasfajger@gmail.com>
2025-02-25 22:32:38 +0100

    Add prettier, and lint staged config

--- projects/ng-ui/src/lib/ng-ui.component.ts
+++ projects/ng-ui/src/lib/ng-ui.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ng-ng-ui',
   imports: [],
   template: ` <p>ng-ui works!</p> `,
-  styles: ``
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NgUiComponent {}
